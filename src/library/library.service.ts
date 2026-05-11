dcfb43e11ccdf6bbc1efeecd3bf503f57dc3ee62
Manoj Gurav <manoj.gurav26@gmail.com>
2026-05-11 17:54:15 +0530

    fix: auto-bootstrap default library when empty

--- src/library/library.service.ts
+++ src/library/library.service.ts
@@ -6,11 +6,75 @@ export class LibraryService {
   constructor(private prisma: PrismaService) {}
 
   async getLibraries() {
+    const libraries = await this.prisma.library.findMany({
+      include: {
+        _count: { select: { mediaItems: true } },
+      },
+    });
+
+    if (libraries.length > 0) {
+      return libraries;
+    }
+
+    await this.ensureDefaultLibraryData();
+
     return this.prisma.library.findMany({
       include: {
         _count: { select: { mediaItems: true } },
       },
     });
+  }
+
+  private async ensureDefaultLibraryData() {
+    const library = await this.prisma.library.upsert({
+      where: { name: 'Sample Movies' },
+      update: {},
+      create: {
+        name: 'Sample Movies',
+        type: 'movies',
+        path: '/media/movies',
+      },
+    });
+
+    let media = await this.prisma.mediaItem.findFirst({
+      where: {
+        libraryId: library.id,
+        title: 'Test Video',
+        type: 'movie',
+      },
+    });
+
+    if (!media) {
+      media = await this.prisma.mediaItem.create({
+        data: {
+          title: 'Test Video',
+          type: 'movie',
+          libraryId: library.id,
+          duration: 603,
+          codec: 'h264',
+          year: 2025,
+        },
+      });
+    }
+
+    const variant = await this.prisma.hlsVariant.findFirst({
+      where: {
+        mediaItemId: media.id,
+        quality: '480p',
+      },
+    });
+
+    if (!variant) {
+      await this.prisma.hlsVariant.create({
+        data: {
+          mediaItemId: media.id,
+          quality: '480p',
+          bitrate: 1500,
+          manifestUrl:
+            'https://pub-0439985b7dab4c45b97f07d23ded7462.r2.dev/TestVideo/index.m3u8',
+        },
+      });
+    }
   }
 
   async getLibraryContent(libraryId: number) {
